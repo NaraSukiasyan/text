b777ec4b199d19743c698c3dcaf6959f4ff46313
newuser <narasukiasyanb93@mail.ru>
2022-03-24 20:51:46 +0400

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int nums[]={0,1,2,3,4,5,6,7,8,9};
+        System.out.println("num");
+
+    }
+}
